89123eec494f840389ec31c3f1948bee05ca23ec
MAYTINH-TO4QHOV\Administrator <vuthienan178@gmail.com>
2017-10-29 13:30:55 +0700

    new commit

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const openCamera = require('./openCamera.js');
+const $ = require('jquery');
+//openCamera();
+const Peer = require('simple-peer');
+const p = new Peer({initiator: location.hash === '#1',trickle : false});
 
-openCamera();
+p.on('signal', token =>{
+    $('#txtMySignal').val(JSON.stringify(token));
+});
+
+
 
 console.log('xin chao cac ban');
