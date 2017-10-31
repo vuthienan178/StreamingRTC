b4645df6af66c334d83834cc2b3057620d2191f9
MAYTINH-TO4QHOV\Administrator <vuthienan178@gmail.com>
2017-10-31 21:22:08 +0700

    deploy

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const app = express();
-app.set('view engine','ejs');
-app.set('views','./views');
-app.use(express.static('public'));
-app.get('/',(req,res) => res.render('home'));
-
- app.listen(3000,() => console.log('Server started'));
